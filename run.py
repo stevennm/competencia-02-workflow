1d026e043c1893ff44b86b3c29d710971dedeb70
Steven Martins <ruben.martins@mercadolibre.com>
2025-11-09 22:11:37 -0300

    indent fix

--- run.py
+++ run.py
@@ -135,9 +135,9 @@ def main():
         df = add_rf_features(df, PARAM, campos_buenos)
         
 
-	print("Saving final dataset")
-	df.write_parquet("data/final_dataset.parquet")
-	print("Final dataset saved")
+        print("Saving final dataset")
+        df.write_parquet("data/final_dataset.parquet")
+        print("Final dataset saved")
 
         # =====================================================================
         # STEP 5: PREPARE DATA FOR TRAINING
