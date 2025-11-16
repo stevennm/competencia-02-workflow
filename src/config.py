4581145f42fb0ef4076ebffbbac7d9eb0cd766bf
Steven <rstevenmartins@gmail.com>
2025-11-16 03:00:04 +0000

    ran experiment step predict 202104 and preconfig 202106 step

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ Configuration file for zLightGBM workflow
 
 PARAM = {
     # Experiment configuration
-    "experimento": "experimento_colab_step_202104",
+    "experimento": "experimento_colab_step_202106",
     "semilla_primigenia": 123479,
     
     # Bucket configuration (for VM storage)
@@ -30,9 +30,9 @@ PARAM = {
             201907, 201908, 201909, 201910, 201911, 201912,
             202001, 202002, 202003, 202004, 202005, 202006,
             202007, 202008, 202009, 202010, 202011, 202012,
-            202101, 202102 #, 202103, 202104
+            202101, 202102 , 202103, 202104
             ], # From notebook
-            "future": [202104],
+            "future": [202106],
             "undersampling": 0.10,  # Conservative (50%)
             "ksemillerio": 1,  # Only 1 model (zLightGBM is robust enough)
             
